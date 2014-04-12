fffa31fe4b427f23957d81feddf56b334f8b8ee2
Egor Andreevici <andreevich.egor@gmail.com>
2014-04-12 11:51:12 +0300

    - Some more attempts to make NFC work

--- app/src/main/java/com/cavemen/inception/nfc/CavemenNFCReaderActivity.java
+++ app/src/main/java/com/cavemen/inception/nfc/CavemenNFCReaderActivity.java
@@ -7,8 +7,7 @@ import android.nfc.Tag;
 import android.nfc.tech.IsoDep;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.cavemen.inception.R;
@@ -23,38 +22,38 @@ import com.cavemen.inception.R;
 public class CavemenNFCReaderActivity extends Activity implements CavemenIsoDepTransceiver.OnMessageExchangeListener,
         NfcAdapter.ReaderCallback {
 
+    private static final String TAG = CavemenNFCReaderActivity.class.getSimpleName();
+
     private NfcAdapter nfcAdapter;
 
-    private boolean isInReaderMode;
-
     private TextView messageConsole;
-    private Button toggleNfcModeBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nfc_reader);
         messageConsole = (TextView) findViewById(R.id.console);
-        toggleNfcModeBtn = (Button) findViewById(R.id.toggle_nfc_mode);
 
         nfcAdapter = NfcAdapter.getDefaultAdapter(this);
     }
 
-    public void toggleReaderMode(View v) {
-        if (isInReaderMode) {
-            nfcAdapter.disableReaderMode(this);
-            isInReaderMode = false;
-            toggleNfcModeBtn.setText(R.string.enable_reader_mode);
-        } else {
-            nfcAdapter.enableReaderMode(this, this, NfcAdapter.FLAG_READER_NFC_A | NfcAdapter.FLAG_READER_SKIP_NDEF_CHECK,
-                    null);
-            isInReaderMode = true;
-            toggleNfcModeBtn.setText(R.string.disable_reader_mode);
-        }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "enabling reader mode");
+        nfcAdapter.enableReaderMode(this, this, NfcAdapter.FLAG_READER_NFC_A | NfcAdapter.FLAG_READER_SKIP_NDEF_CHECK, null);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "disabling reader mode");
+        nfcAdapter.disableReaderMode(this);
     }
 
     @Override
     public void onTagDiscovered(Tag tag) {
+        Log.d(TAG, "tag discovered");
         IsoDep isoDep = IsoDep.get(tag);
         CavemenIsoDepTransceiver transceiver = new CavemenIsoDepTransceiver(isoDep, this);
         Thread thread = new Thread(transceiver);
