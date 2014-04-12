b2b3162a962215e5119152e0343ff22a4ddf13fc
Gleb Godonoga <gleb.godonoga@gmail.com>
2014-04-12 18:10:55 +0300

    Floor list updates

--- app/src/main/java/com/cavemen/inception/ui/fragment/VenueFragment.java
+++ app/src/main/java/com/cavemen/inception/ui/fragment/VenueFragment.java
@@ -1,18 +1,6 @@
 package com.cavemen.inception.ui.fragment;
 
 import android.app.Fragment;
-import android.content.Intent;
-import android.view.View;
-import android.widget.Button;
-
-import com.cavemen.inception.R;
-import com.cavemen.inception.ui.FloorActivity;
-import com.cavemen.inception.ui.FloorActivity_;
-
-import org.androidannotations.annotations.AfterViews;
-import org.androidannotations.annotations.Click;
-import org.androidannotations.annotations.EFragment;
-import org.androidannotations.annotations.FragmentArg;
 import android.widget.ListView;
 
 import com.cavemen.inception.R;
@@ -59,7 +47,7 @@ public class VenueFragment extends Fragment {
     }
 
     public String getCurrentFloor() {
-        return (String)floorsAdapter.getItem((int)currentFloorIndex);
+        return (String) floorsAdapter.getItem((int) currentFloorIndex);
     }
 
 
