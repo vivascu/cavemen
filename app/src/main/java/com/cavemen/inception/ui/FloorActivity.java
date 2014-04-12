dcaabb24540246bdd0022af82878b11f70593b1a
Victor Ivascu <victor.ivascu@endava.com>
2014-04-13 01:26:29 +0300

    new Icon

--- app/src/main/java/com/cavemen/inception/ui/FloorActivity.java
+++ app/src/main/java/com/cavemen/inception/ui/FloorActivity.java
@@ -1,5 +1,8 @@
 package com.cavemen.inception.ui;
 
+import android.app.DialogFragment;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
@@ -12,6 +15,7 @@ import com.cavemen.inception.R;
 import com.cavemen.inception.model.Floor;
 import com.cavemen.inception.model.Table;
 import com.cavemen.inception.model.TableStatus;
+import com.cavemen.inception.ui.fragment.TableDialogFragment_;
 import com.cavemen.inception.ui.view.TableView;
 import com.parse.ParseException;
 import com.parse.ParseObject;
@@ -52,6 +56,10 @@ public class FloorActivity extends BaseActivity implements PhotoViewAttacher.OnM
 
     @AfterViews
     public void afterView() {
+        Drawable bitmap = getResources().getDrawable(R.drawable.caveplan);
+        imageView.setImageDrawable(bitmap);
+        mAttacher = new PhotoViewAttacher(imageView);
+        mAttacher.setOnMatrixChangeListener(this);
         getTables();
     }
 
@@ -76,7 +84,7 @@ public class FloorActivity extends BaseActivity implements PhotoViewAttacher.OnM
 
     @UiThread
     public void drawTables(List<Table> list) {
-        if(mRect!=null){
+        if (mRect != null) {
             onMatrixChanged(mRect);
         }
     }
@@ -84,18 +92,9 @@ public class FloorActivity extends BaseActivity implements PhotoViewAttacher.OnM
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        Drawable bitmap = getResources().getDrawable(R.drawable.caveplan);
-        imageView.setImageDrawable(bitmap);
-        mAttacher = new PhotoViewAttacher(imageView);
-        mAttacher.setOnMatrixChangeListener(this);
-
 
     }
+
 
     public void addDeskView(Table table, float imageWidth, int originX, int originY) {
         float ratio;
@@ -125,8 +124,8 @@ public class FloorActivity extends BaseActivity implements PhotoViewAttacher.OnM
         if (container.getChildCount() > mTables.size()) {
             for (int i = 0; i < container.getChildCount(); i++) {
                 View view = container.getChildAt(i);
-                if (view.getId() != R.id.caveplan && view instanceof TableView ) {
-                    updateTablePosition(view,rect.width(), Math.round(rect.centerX() - (rect.width() / 2.0f)), Math.round(rect.centerY() - (rect.height() / 2.0f)));
+                if (view.getId() != R.id.caveplan && view instanceof TableView) {
+                    updateTablePosition(view, rect.width(), Math.round(rect.centerX() - (rect.width() / 2.0f)), Math.round(rect.centerY() - (rect.height() / 2.0f)));
                 }
             }
         } else {
@@ -140,7 +139,8 @@ public class FloorActivity extends BaseActivity implements PhotoViewAttacher.OnM
     private void updateTablePosition(View view, float imageWidth, int originX, int originY) {
         TableView tableView = (TableView) view;
         Table table = (Table) tableView.getTag();
-        float ratio =  imageWidth / 1267.0f;;
+        float ratio = imageWidth / 1267.0f;
+        ;
         FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) view.getLayoutParams();
         params.width = Math.round(tableView.intialWeight * ratio);
         params.height = Math.round((tableView.intialHeight * ratio));
@@ -152,15 +152,36 @@ public class FloorActivity extends BaseActivity implements PhotoViewAttacher.OnM
 
     @Override
     public void onClick(View view) {
-        if(view instanceof TableView){
+        if (view instanceof TableView) {
             Table table = (Table) view.getTag();
-            view.setActivated(!view.isActivated());
-            if (view.isActivated()){
-                if (previouslyActivatedView!=null){
+            if (!view.isActivated()) {
+                if (previouslyActivatedView != null) {
                     previouslyActivatedView.setActivated(false);
                 }
                 previouslyActivatedView = view;
             }
+
+
+            showDialog(table);
+            view.setActivated(!view.isActivated());
+
         }
+    }
+
+    void showDialog(Table table) {
+
+        // DialogFragment.show() will take care of adding the fragment
+        // in a transaction.  We also want to remove any currently showing
+        // dialog, so make our own transaction and take care of that here.
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        Fragment prev = getFragmentManager().findFragmentByTag("dialog");
+        if (prev != null) {
+            ft.remove(prev);
+        }
+        ft.addToBackStack(null);
+
+        // Create and show the dialog.
+        DialogFragment newFragment = TableDialogFragment_.builder().mTable(table).build();
+        newFragment.show(ft, "dialog");
     }
 }
