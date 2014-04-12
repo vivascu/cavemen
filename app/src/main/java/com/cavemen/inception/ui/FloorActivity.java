f1bcdff575e09c01603ffe6b96e5194b4d559f71
Victor Ivascu <victor.ivascu@endava.com>
2014-04-12 22:06:46 +0300

    added photoview

--- app/src/main/java/com/cavemen/inception/ui/FloorActivity.java
+++ app/src/main/java/com/cavemen/inception/ui/FloorActivity.java
@@ -1,5 +1,8 @@
 package com.cavemen.inception.ui;
 
+import android.graphics.RectF;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
@@ -12,11 +15,13 @@ import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 
+import uk.co.senab.photoview.PhotoViewAttacher;
+
 /**
  * Created by vivascu on 4/12/2014.
  */
 @EActivity(R.layout.floor_fragment_layout)
-public class FloorActivity extends BaseActivity {
+public class FloorActivity extends BaseActivity implements PhotoViewAttacher.OnMatrixChangedListener {
 
     @ViewById(R.id.Table)
     TableView mTableView;
@@ -24,6 +29,7 @@ public class FloorActivity extends BaseActivity {
     ImageView imageView;
     @ViewById(R.id.table_container)
     FrameLayout container;
+    PhotoViewAttacher mAttacher;
 
 
     @AfterViews
@@ -32,15 +38,24 @@ public class FloorActivity extends BaseActivity {
     }
 
     @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
-
+        Drawable bitmap = getResources().getDrawable(R.drawable.caveplan);
+        imageView.setImageDrawable(bitmap);
+        mAttacher = new PhotoViewAttacher(imageView);
+        mAttacher.setOnMatrixChangeListener(this);
         addDeskView(null, 1022, 136);
         addDeskView(null, 1022, 216);
         addDeskView(null, 1022, 296);
         addDeskView(null, 975, 136);
         addDeskView(null, 975, 216);
         addDeskView(null, 975, 296);
+
     }
 
     public void addDeskView(Table table, int x, int y) {
@@ -54,13 +69,17 @@ public class FloorActivity extends BaseActivity {
         view.setOccupied(false);
         ratio = imageView.getMeasuredWidth() / 1267.0f;
         FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) mTableView.getLayoutParams();
-        params.width = Math.round(mTableView.intialWeight * ratio * displayRatio);
-        params.height = Math.round((mTableView.intialHeight * ratio) * displayRatio);
+        params.width = Math.round(mTableView.intialWeight * ratio );
+        params.height = Math.round((mTableView.intialHeight * ratio));
         //params.setX(Math.round(975*1.5),137,0,0);
         view.setX(Math.round(x * ratio));
         view.setY(Math.round(y * ratio));
         view.setLayoutParams(params);
         view.setTag(table);
-        container.addView(view);
+
+    }
+
+    @Override
+    public void onMatrixChanged(RectF rect) {
     }
 }
